85a403419b47534d250d24adf7df77f008dfede5
LorenzoAblis <lablis228609@gmail.com>
2023-11-17 10:12:21 -0600

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Lorenzo J. Ablis on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
